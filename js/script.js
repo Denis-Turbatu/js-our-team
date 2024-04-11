41efd5be6e29e49614161aeeb54e1d32214e3029
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-11 14:24:20 +0200

    feat: array with objects

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const team = [
+  {
+    name: "Wayne Barnett",
+    profession: "Founder & CEO",
+    imgPath: "wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    name: "Angela Caroll",
+    profession: "Chief Editor",
+    imgPath: "angela-caroll-chief-editor.jpg"
+  },
+  {
+    name: "Walter Gordon",
+    profession: "Office Manager",
+    imgPath: "walter-gordon-office-manager.jpg"
+  },
+  {
+    name: "Angela Lopez",
+    profession: "Social Media Manager",
+    imgPath: "angela-lopez-social-media-manager.jpg"
+  },
+  {
+    name: "Scott Estrada",
+    profession: "Developer",
+    imgPath: "scott-estrada-developer.jpg"
+  },
+  {
+    name: "Barbara Ramos",
+    profession: "Graphic Designer",
+    imgPath: "barbara-ramos-graphic-designer.jpg"
+  }
+];
